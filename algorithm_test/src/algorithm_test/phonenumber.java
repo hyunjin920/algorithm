108bed1d5367b2820cb127b2a502b3541164f7a8
hyunjin920 <hyunjin920@naver.com>
2021-06-10 16:43:14 +0900

    알고리즘2 커밋

---
+++ algorithm_test/src/algorithm_test/phonenumber.java
@@ -0,0 +1,18 @@
+package algorithm_test;
+import java.util.HashMap;
+
+public class phonenumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	class Solution {
+		//["119", "97674223", "1195524421"] - 테스트 케이스 
+	    public boolean solution(String[] phone_book) {
+	        boolean answer = true;
+	        return answer;
+	    }
+	}
+}
